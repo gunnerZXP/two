7842e9c16558782aa273bf1217178ce7f1ad9e96
zxp <kapokyy@163.com>
2012-12-05 12:55:09 +0800

    change routes' '

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sample::Application.routes.draw do
+  root :to => 'home#index'
   resources :posts, :articles do
     resources :comments
   end
@@ -54,7 +55,7 @@ Sample::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
