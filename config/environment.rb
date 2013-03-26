09482114ef0a6a86a7bb1b4c9237347d274fa0cd
zxp <zxp@zxp-K53SV.(none)>
2013-03-26 22:04:43 +0800

    change time_zone

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-ActiveSupport::Deprecation.silence do
-  Selfcare::Application.initialize!
+Zxp::Application.initialize!
 end
 
